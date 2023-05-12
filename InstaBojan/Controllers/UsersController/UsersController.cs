01acf908c5c0c5053380f2bd13aeb17e4f8981f8
Boki89 <bojanstupar1989@gmail.com>
2023-05-12 13:40:21 +0200

    Added two methods in UsersController

---
+++ InstaBojan/Controllers/UsersController/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstaBojan.Controllers.UsersController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
