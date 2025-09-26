23ba82f51dc630fe3b99b7f8a3a83689f4f64b8d
BojanStupar089 <117210357+BojanStupar089@users.noreply.github.com>
2025-09-26 15:55:07 +0200

    Update User.cs

--- InstaBojan.Core/Models/User.cs
+++ InstaBojan.Core/Models/User.cs
@@ -13,9 +13,9 @@ namespace InstaBojan.Core.Models
     {
 
         public int Id { get; set; }
-        public  string UserName { get; set; }
-        public  string Password { get; set; }
-        public string Email { get; set; }
+        public  string?UserName { get; set; }
+        public  string?Password { get; set; }
+        public string? Email { get; set; }
         public Role Role { get; set; }
 
         
