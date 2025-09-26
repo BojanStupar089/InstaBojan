b91c74cfa131e290c16856be05069cafe2e20cf2
BojanStupar089 <117210357+BojanStupar089@users.noreply.github.com>
2025-09-26 15:56:54 +0200

    Update Register.cs

--- InstaBojan.Core/Security/Register.cs
+++ InstaBojan.Core/Security/Register.cs
@@ -3,13 +3,13 @@
     public class Register
     {
 
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-        public string ProfileName { get; set; }
-        public string Gender { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? ProfileName { get; set; }
+        public string? Gender { get; set; }
         public DateTime BirthDay { get; set; }
-        public string ProfilePicture { get; set; }
+        public string? ProfilePicture { get; set; }
 
 
 
